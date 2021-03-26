a7d584de550091b1671c2a098637aff5404c9231
Pedro Augusto <peedroaugustto@hotmail.com>
2021-03-26 16:05:09 -0300

    Classe Position

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Ola");
+		
+		
+		Position p = new Position(3,5);
+		System.out.println(p);
 	}
 
 }
